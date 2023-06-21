bd17a017587e4cafa365bf098aee43a2d9b93810
DasBaka <122975603+DasBaka@users.noreply.github.com>
2023-06-21 18:40:55 +0200

    JS DOC

--- js/classes/models/throwable-object.class.js
+++ js/classes/models/throwable-object.class.js
@@ -13,7 +13,10 @@ class ThrowableObject extends JumpableObject {
       this.loadAnimations();
    }
 
-   setWorld() {
+   /**
+    * Sets world to itself to calculate its current position + speed when it'll be generated.
+    */
+   setWorld(world) {
       this.world = world;
       let char = this.world.character;
       this.y = char.y + char.h * 0.5;
@@ -21,6 +24,11 @@ class ThrowableObject extends JumpableObject {
       this.setBottleAcceleration(char);
    }
 
+   /**
+    * Defines position adjustment depending on the throw direction.
+    * @param {boolean} otherDir - face direction of the character
+    * @returns - number (to recalculate x position)
+    */
    bottlePosition(otherDir) {
       if (otherDir) {
          return bottleLeftSidePosition;
@@ -29,28 +37,46 @@ class ThrowableObject extends JumpableObject {
       }
    }
 
+   /**
+    * Sets parameter to define the throw direction.
+    * @param {object} char - character class object
+    */
    setBottleAcceleration(char) {
       this.rightAcceleration = char.validateRight();
       this.leftAcceleration = char.validateLeft();
       this.otherDirection = char.otherDirection;
    }
 
+   /**
+    * Left move.
+    */
    left() {
       if (this.speed != 0 && this.otherDirection) {
          this.moveLeft();
       }
    }
 
+   /**
+    * Right move.
+    */
    right() {
       if (this.speedX != 0 && !this.otherDirection) {
          this.moveRight();
       }
    }
 
+   /**
+    * Checks is the Bottle can be thrown.
+    * @returns - boolean
+    */
    validateOnGround() {
       return THROW && this.speedX_rel == 0 && this.health != 0;
    }
 
+   /**
+    * Fires, if object condition is true and starts a throw alias "jump".
+    * Triggers another function depending on the characters movement.
+    */
    startJump() {
       if (this.validateOnGround()) {
          if (this.world.character.validateInAir()) {
@@ -63,29 +89,49 @@ class ThrowableObject extends JumpableObject {
       }
    }
 
+   /**
+    * Bottle's stats while thrown in mid air.
+    */
    throwMidAir() {
       this.speedX_rel = pepeSpeed * 2.0;
       this.speedY_rel = 0.0125;
    }
 
+   /**
+    * Bottle's stats while thrown while moving on the ground.
+    */
    throwWithSpeed() {
       this.speedX_rel = pepeSpeed * 4;
       this.speedY_rel = 0.007;
    }
 
+   /**
+    * Bottle's stats while thrown while idle.
+    */
    throwOnIdle() {
       this.speedX_rel = pepeSpeed * 1.5;
       this.speedY_rel = 0.01;
    }
 
+   /**
+    * Checks if throwable-object still flies.
+    * @returns - boolean
+    */
    validateInAir() {
       return this.health != 0;
    }
 
+   /**
+    * Checks for a collision with the ground or indirectly enemy (object's health = 0).
+    * @returns - boolean
+    */
    validateBackOnGround() {
       return this.y > groundLevel - this.h || this.health == 0;
    }
 
+   /**
+    * Stats on collision.
+    */
    isHit() {
       this.health = 0;
       this.speedY_rel = 0;
@@ -93,6 +139,9 @@ class ThrowableObject extends JumpableObject {
       this.refreshSpeed();
    }
 
+   /**
+    * Individual end of this objects gravity. (also see: {@link JumpableObject.endGravity})
+    */
    endGravity() {
       this.isHit();
       this.y = this.y + 0.33 * this.h;
@@ -100,6 +149,11 @@ class ThrowableObject extends JumpableObject {
       this.endInterval(this.jumpInterval);
    }
 
+   /**
+    * This object is always "resulting".
+    * Relevant for its animation.
+    * @returns - true
+    */
    isFalling() {
       return true;
    }
